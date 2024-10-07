adb4155613d1a81e3f3747d6b66a8c19c90a9fcc
maxstain <firaschabchoub@hotmail.com>
2024-10-07 14:59:07 +0100

    Added the Testing and try catch to test.

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -7,7 +7,7 @@ class MainTest {
     @org.junit.jupiter.api.Test
     public void main() {
         try {
-            Employe employe = new Employe(1, "Mohamed", "Tunis", 200);
+            Employe employe = new Employe(1, "Mohamed", "Tunis", 300);
             if (employe.calculeSalaire() == 180 * 5 + (200 - 180) * 1.15) {
                 System.out.println("Test cassier.calculeSalaire() passed");
             } else {
