95e00a406eb52789284c4c247006f91d2ebdab47
maxstain <firaschabchoub@hotmail.com>
2024-10-07 14:57:43 +0100

    Added the Testing and try catch to test.

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -1,11 +1,20 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import tn.tuniprod.gestionmagasin.Models.*;
 
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    void main() {
-
+    public void main() {
+        try {
+            Employe employe = new Employe(1, "Mohamed", "Tunis", 200);
+            if (employe.calculeSalaire() == 180 * 5 + (200 - 180) * 1.15) {
+                System.out.println("Test cassier.calculeSalaire() passed");
+            } else {
+                throw new Exception("Test cassier.calculeSalaire() failed");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
